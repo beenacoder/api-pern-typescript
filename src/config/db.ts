fd12c3f1402ce18798cec6c35c247582b1330095
Ariel Cabeza <cervanteslibch@gmail.com>
2024-07-20 21:43:26 -0300

    adding product to database

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import {Sequelize} from 'sequelize-typescript';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import Product from '../models/Product.model';
 
 
 dotenv.config();
@@ -12,5 +13,7 @@ const __dirname = dirname(__fileName);
 const db = new Sequelize(process.env.DATABASE_URL!, {
     models:[join(__dirname + '/../models/**/*.model.ts')],
 });
+
+db.addModels([Product]);
 
 export default db;
--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
+import Product from "../models/Product.model";
 
 
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
     console.log(req.body)
-    res.send('Create Product');
+    // const product = new Product(req.body);
+    // const savedProduct = await product.save();
+    const product = await Product.create(req.body);
+    res.send({data: product});
 }
