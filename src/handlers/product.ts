1b320234adad9c8d91ba7c2914a1914787fb5b00
Ariel Cabeza <cervanteslibch@gmail.com>
2024-08-26 16:58:28 -0300

    CRUD listo

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,11 +1,89 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
-
+//------------------------POST /product
 export const createProduct = async (req: Request, res: Response) => {
-    console.log(req.body)
-    // const product = new Product(req.body);
-    // const savedProduct = await product.save();
-    const product = await Product.create(req.body);
-    res.send({data: product});
+    try {
+        // const product = new Product(req.body);
+        // const savedProduct = await product.save();
+        const product = await Product.create(req.body);
+        res.json({ data: product });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+//-----------------------------GET /product
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['id', 'DESC']
+            ],
+            attributes: {
+                exclude: ['createdAt', 'updatedAt', 'availability']
+            }
+        });
+        if (!products) { res.status(404).send({ data: "No products found" }); }
+        res.json({ data: products });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//-------------------------------GET /product/:id
+export const getProductById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const productById = await Product.findByPk(id);
+        if (!productById) { res.status(404).send({ error: "No product found" }); }
+        res.json({ data: productById });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//-------------------------------PUT/UPDATE /product
+//PUT reemplaza el registro con lo que le pasemos, es decir si solo le pasamos un solo campo lo demas los pasa en vacío.
+//Por eso usamos product.update para protegernos y que se llenen todos los campos.
+export const updateProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByPk(id);
+        if (!product) { res.status(404).send({ error: "No product found" }); }
+        await product.update(req.body);
+        const updatedProduct = await product.save();
+        res.json({ data: updatedProduct });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//-------------------------------PATCH /product
+export const updateAvailability = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByPk(id);
+        if (!product) { res.status(404).send({ error: "No product found" }); }
+        product.availability = !product.dataValues.availability;
+        await product.save();
+        res.json({ data: product });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//-------------------------------DELETE /product
+export const deleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByPk(id);
+        if (!product) { res.status(404).send({ error: "No product found" }); }
+        await product.destroy();
+        res.json({ data: "Product deleted" });
+    } catch (error) {
+        console.log(error);
+    }
 }
--- src/routers.ts
+++ src/routers.ts
@@ -1,24 +1,54 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from "./handlers/product";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "./middlewares/errors";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Get World');
-});
+router.post('/',
+    //Validación
+    body('name')
+        .notEmpty().withMessage('Debes ingresar un nombre para producto'),
+    body('price')
+        .isNumeric().withMessage("Debe ser un número")
+        .notEmpty().withMessage('Debes ingresar un precio para el producto')
+        .custom(val => val > 0).withMessage('El valor debe ser mayor que 0'),
+    handleInputErrors,
+    createProduct);
 
-router.post('/', createProduct);
+router.get('/',
+    getProducts
+);
 
-router.put('/', (req, res) => {
-    res.send('Put World');
-});
+router.get('/:id',
+    param('id').isInt().withMessage('El id debe ser un número'),
+    handleInputErrors,
+    getProductById
+);
 
-router.patch('/', (req, res) => {
-    res.send('PATCH World');
-});
 
-router.delete('/', (req, res) => {
-    res.send('DELETE World');
-});
+router.put('/:id',
+    param('id').isInt().withMessage('El id debe ser un número'),
+    body('name')
+        .notEmpty().withMessage('Debes ingresar un nombre para producto'),
+    body('price')
+        .isNumeric().withMessage("Debe ser un número")
+        .notEmpty().withMessage('Debes ingresar un precio para el producto')
+        .custom(val => val > 0).withMessage('El valor debe ser mayor que 0'),
+    body('availability')
+        .isBoolean().withMessage("Debe ser un valor booleano"),
+    handleInputErrors,
+    updateProduct);
+
+router.patch('/:id',
+    param('id').isInt().withMessage('El id debe ser un número'),
+    handleInputErrors,
+    updateAvailability);
+
+router.delete('/:id', 
+    param('id').isInt().withMessage('El id debe ser un número'),
+    handleInputErrors,
+    deleteProduct
+);
 
 export default router;
